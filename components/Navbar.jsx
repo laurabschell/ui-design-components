708c3720f148345fd31cf3a9b52e801f9c403668
Laura Schell <laurabelenschell@gmail.com>
2021-10-05 12:46:40 -0300

    menu with semantic added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import Link from 'next/link'
-import { Button, Container, Segment } from 'semantic-ui-react'
+import { Button, Container, Menu, Segment } from 'semantic-ui-react'
 import styles from '../styles/Home.module.scss'
 
 function Navbar() {
     return (
-        <Segment padded>
-            <h2>Fit Tool</h2>
-            <Container className={styles.button_container}>
-                <Button>
-                    <Link href="/">Home</Link>
-                </Button>
-                <Button>
-                    <Link href="/about">About</Link>
-                </Button>
-                <Button>
-                    <Link href="/contact">Contact</Link>
-                </Button>
-            </Container>
-        </Segment>
+        // <Segment padded>
+        //     <h2>Fit Tool</h2>
+        //     <Container className={styles.button_container}>
+        //         <Button>
+        //             <Link href="/">Home</Link>
+        //         </Button>
+        //         <Button>
+        //             <Link href="/about">About</Link>
+        //         </Button>
+        //         <Button>
+        //             <Link href="/contact">Contact</Link>
+        //         </Button>
+        //     </Container>
+        // </Segment>
+        <Menu>
+        <Menu.Item>
+          <Link href="/">Home</Link>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Link href="/about">About</Link>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Link href="/contact">Contact</Link>
+        </Menu.Item>
+      </Menu>
     )
 }
 
